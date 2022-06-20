aaa46af7a3292e0b11351fd4ee761bb6cf63b000
duongan2412 <duongan2412@gmail.com>
2022-06-20 22:23:58 +0700

    update img url

--- src/Components/BaiTapLayoutComponent/Item.jsx
+++ src/Components/BaiTapLayoutComponent/Item.jsx
@@ -7,7 +7,7 @@ export default class Item extends Component {
                 <div className="row gx-lg-5">
                     <div className="col-md-3">
                         <div className="card" style={{ width: 240 }}>
-                            <img className="card-img-top" src={require('./../../img/500x325.png')} alt="Cardimage" style={{ width: '100%' }} />
+                            <img className="card-img-top" src="./img/500x325.png" alt="Cardimage" style={{ width: '100%' }} />
                             <div className="card-body">
                                 <h4 className="card-title">Cart Title</h4>
                                 <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, amet.</p>
@@ -17,7 +17,7 @@ export default class Item extends Component {
                     </div>
                     <div className="col-md-3">
                         <div className="card" style={{ width: 240 }}>
-                            <img className="card-img-top" src={require('./../../img/500x325.png')} alt="Cardimage" style={{ width: '100%' }} />
+                            <img className="card-img-top" src="./img/500x325.png" alt="Cardimage" style={{ width: '100%' }} />
                             <div className="card-body">
                                 <h4 className="card-title">Cart Title</h4>
                                 <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, amet.</p>
@@ -27,7 +27,7 @@ export default class Item extends Component {
                     </div>
                     <div className="col-md-3">
                         <div className="card" style={{ width: 240 }}>
-                            <img className="card-img-top" src={require('./../../img/500x325.png')} alt="Cardimage" style={{ width: '100%' }} />
+                            <img className="card-img-top" src="./img/500x325.png" alt="Cardimage" style={{ width: '100%' }} />
                             <div className="card-body">
                                 <h4 className="card-title">Cart Title</h4>
                                 <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, amet.</p>
@@ -37,7 +37,7 @@ export default class Item extends Component {
                     </div>
                     <div className="col-md-3">
                         <div className="card" style={{ width: 240 }}>
-                            <img className="card-img-top" src={require('./../../img/500x325.png')} alt="Cardimage" style={{ width: '100%' }} />
+                            <img className="card-img-top" src="./img/500x325.png" alt="Cardimage" style={{ width: '100%' }} />
                             <div className="card-body">
                                 <h4 className="card-title">Cart Title</h4>
                                 <p className="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi, amet.</p>
